0455f40eda146e1cb7c8493b89d1cf2aaa193860
yamaankhan20 <khanyamaan1@gmail.com>
2024-11-02 02:09:33 +0500

    Merge remote-tracking branch 'Master/master'
    # Conflicts:
    #	package.json

--- src/App.js
+++ src/App.js
@@ -24,67 +24,67 @@ function App() {
         document.body.appendChild(script);
     };
 
-    useEffect(() => {
-        const cssFiles = [
-            "./assets/css/font-awesome.css",
-            "./assets/css/vendors/icofont.css",
-            "./assets/css/vendors/themify.css",
-            "./assets/css/vendors/flag-icon.css",
-            "./assets/css/vendors/feather-icon.css",
-            "./assets/css/vendors/slick.css",
-            "./assets/css/vendors/slick-theme.css",
-            "./assets/css/vendors/scrollbar.css",
-            "./assets/css/vendors/animate.css",
-            "./assets/css/vendors/datatables.css",
-            "./assets/css/vendors/date-range-picker/flatpickr.min.css",
-            "./assets/css/vendors/bootstrap.css",
-            "./assets/css/style.css",
-            "./assets/css/color-1.css",
-            "./assets/css/responsive.css",
-        ];
-
-        const jsFiles = [
-            "./assets/js/jquery.min.js",
-            "./assets/js/bootstrap/bootstrap.bundle.min.js",
-            "./assets/js/script.js",
-            "./assets/js/icons/feather-icon/feather.min.js",
-            "./assets/js/icons/feather-icon/feather-icon.js",
-            "./assets/js/scrollbar/simplebar.js",
-            "./assets/js/scrollbar/custom.js",
-            "./assets/js/config.js",
-            "./assets/js/sidebar-menu.js",
-            "./assets/js/sidebar-pin.js",
-            "./assets/js/slick/slick.min.js",
-            "./assets/js/slick/slick.js",
-            "./assets/js/header-slick.js",
-            "./assets/js/notify/bootstrap-notify.min.js",
-            "./assets/js/datatable/datatables/jquery.dataTables.min.js",
-            "./assets/js/datatable/datatables/datatable.custom.js",
-            "./assets/js/datatable/datatables/datatable.custom1.js",
-            "./assets/js/datepicker/date-range-picker/moment.min.js",
-            "./assets/js/datepicker/date-range-picker/datepicker-range-custom.js",
-            "./assets/js/typeahead/handlebars.js",
-            "./assets/js/typeahead/typeahead.bundle.js",
-            "./assets/js/typeahead/typeahead.custom.js",
-            "./assets/js/typeahead-search/handlebars.js",
-            "./assets/js/typeahead-search/typeahead-custom.js",
-            "./assets/js/height-equal.js",
-            "./assets/js/animation/wow/wow.min.js",
-        ];
-
-        // Load CSS files
-        cssFiles.forEach(loadCSS);
-
-        // Load JS files after DOMContentLoaded
-        const loadAllScripts = () => {
-            jsFiles.forEach(loadJS);
-        };
-
-        document.addEventListener("DOMContentLoaded", loadAllScripts);
-        return () => {
-            document.removeEventListener("DOMContentLoaded", loadAllScripts);
-        };
-    }, []);
+    // useEffect(() => {
+    //     const cssFiles = [
+    //         "./assets/css/font-awesome.css",
+    //         "./assets/css/vendors/icofont.css",
+    //         "./assets/css/vendors/themify.css",
+    //         "./assets/css/vendors/flag-icon.css",
+    //         "./assets/css/vendors/feather-icon.css",
+    //         "./assets/css/vendors/slick.css",
+    //         "./assets/css/vendors/slick-theme.css",
+    //         "./assets/css/vendors/scrollbar.css",
+    //         "./assets/css/vendors/animate.css",
+    //         "./assets/css/vendors/datatables.css",
+    //         "./assets/css/vendors/date-range-picker/flatpickr.min.css",
+    //         "./assets/css/vendors/bootstrap.css",
+    //         "./assets/css/style.css",
+    //         "./assets/css/color-1.css",
+    //         "./assets/css/responsive.css",
+    //     ];
+    //
+    //     const jsFiles = [
+    //         "./assets/js/jquery.min.js",
+    //         "./assets/js/bootstrap/bootstrap.bundle.min.js",
+    //         "./assets/js/script.js",
+    //         "./assets/js/icons/feather-icon/feather.min.js",
+    //         "./assets/js/icons/feather-icon/feather-icon.js",
+    //         "./assets/js/scrollbar/simplebar.js",
+    //         "./assets/js/scrollbar/custom.js",
+    //         "./assets/js/config.js",
+    //         "./assets/js/sidebar-menu.js",
+    //         "./assets/js/sidebar-pin.js",
+    //         "./assets/js/slick/slick.min.js",
+    //         "./assets/js/slick/slick.js",
+    //         "./assets/js/header-slick.js",
+    //         "./assets/js/notify/bootstrap-notify.min.js",
+    //         "./assets/js/datatable/datatables/jquery.dataTables.min.js",
+    //         "./assets/js/datatable/datatables/datatable.custom.js",
+    //         "./assets/js/datatable/datatables/datatable.custom1.js",
+    //         "./assets/js/datepicker/date-range-picker/moment.min.js",
+    //         "./assets/js/datepicker/date-range-picker/datepicker-range-custom.js",
+    //         "./assets/js/typeahead/handlebars.js",
+    //         "./assets/js/typeahead/typeahead.bundle.js",
+    //         "./assets/js/typeahead/typeahead.custom.js",
+    //         "./assets/js/typeahead-search/handlebars.js",
+    //         "./assets/js/typeahead-search/typeahead-custom.js",
+    //         "./assets/js/height-equal.js",
+    //         "./assets/js/animation/wow/wow.min.js",
+    //     ];
+    //
+    //     // Load CSS files
+    //     cssFiles.forEach(loadCSS);
+    //
+    //     // Load JS files after DOMContentLoaded
+    //     const loadAllScripts = () => {
+    //         jsFiles.forEach(loadJS);
+    //     };
+    //
+    //     document.addEventListener("DOMContentLoaded", loadAllScripts);
+    //     return () => {
+    //         document.removeEventListener("DOMContentLoaded", loadAllScripts);
+    //     };
+    // }, []);
 
     return (
         <Router>
